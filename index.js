81dd666fdf3ed5fb55b7131058ed65321ba17de2
Songkeit <6210742083@student.tu.ac.th>
2022-06-13 15:50:37 +0700

    config connection employee_temp and query contry

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
+//set enviroment about database
 const express = require('express');
 const app = express();
 const mysql = require('mysql');
 const cors = require('cors');
-
 app.use(cors());//อนุญาติและไม่อนุญาติการร้องขอการเข้าถึงทรัพยากรอื่น
 app.use(express.json());
 
-const db = mysql.createConnection({
+//create database connection employee table 
+const employee = mysql.createConnection({
     user:"root",
     host:"localhost",
     password:"songkeit20839",
     database:'employeesystem'
 })
 app.get('/employee' ,(req,res) =>{
-    db.query("SELECT * FROM employee",(err,result) => {
+    employee.query("SELECT * FROM employee",(err,result) => {
         if(err){
             console.log(err)
         }else{
@@ -21,7 +22,6 @@ app.get('/employee' ,(req,res) =>{
         }
     });
 });
-
 app.put('/update',(req,res) =>{
     const id = req.body.id;
     const name = req.body.name;
@@ -29,8 +29,7 @@ app.put('/update',(req,res) =>{
     const country = req.body.country;
     const position = req.body.position;
     const wage = req.body.wage;
-
-    db.query("UPDATE employee SET name = ?, age = ?, country = ?, position = ?, wage = ?  WHERE id = ?",[name,age,country,position,wage,id],//fixable follow column
+    employee.query("UPDATE employee SET name = ?, age = ?, country = ?, position = ?, wage = ?  WHERE id = ?",[name,age,country,position,wage,id],//fixable follow column
     (err,result) =>{
         if(err){
             console.log(err);
@@ -39,6 +38,55 @@ app.put('/update',(req,res) =>{
         }
     });
 })
+
+
+
+
+//--------------------------------------------------------------------------------------------------------------------
+//create database connection employee_temp table 
+const employee_temp = mysql.createConnection({
+    user:"root",
+    host:"localhost",
+    password:"songkeit20839",
+    database:'employeesystem'
+})
+
+app.get('/employee_temp' ,(req,res) =>{
+    employee_temp.query("SELECT * FROM employee_temp",(err,result) => {
+        if(err){
+            console.log(err)
+        }else{
+            res.send(result);
+        }
+    });
+});
+app.put('/update_temp',(req,res) =>{
+    const id = req.body.id;
+    const name = req.body.name;
+    const age = req.body.age;
+    const country = req.body.country;
+    const position = req.body.position;
+    const wage = req.body.wage;
+    employee_temp.query("UPDATE employee_temp SET name = ?, age = ?, country = ?, position = ?, wage = ?  WHERE id = ?",[name,age,country,position,wage,id],//fixable follow column
+    (err,result) =>{
+        if(err){
+            console.log(err);
+        }else{
+            res.send(result);
+        }
+    });
+})
+
+app.get('/employee_temp_check_country' ,(req,res) =>{
+    employee_temp.query("SELECT * FROM employee_temp a LEFT join country on a.country = country.country",(err,result) => {
+        if(err){
+            console.log(err)
+        }else{
+            res.send(result);
+        }
+    });
+});
+
 // set port
 app.listen('3001', () => {
     console.log('server running on port 3001')
