3548f8e187713d0f03dd6402226ea9e2f82c6963
Songkeit <6210742083@student.tu.ac.th>
2022-06-17 16:49:33 +0700

    change B

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 //set enviroment about database
-
-
 const express = require('express');
 const app = express();
 const mysql = require('mysql');
@@ -11,35 +9,35 @@ app.use(express.json());
 
 //create database connection employee table 
 const employee = mysql.createConnection({
-    user:"root",
-    host:"localhost",
-    password:"songkeit20839",
-    database:'employeesystem'
+    user: "root",
+    host: "localhost",
+    password: "songkeit20839",
+    database: 'employeesystem'
 })
-app.get('/employee' ,(req,res) =>{
-    employee.query("SELECT * FROM employee",(err,result) => {
-        if(err){
+app.get('/employee', (req, res) => {
+    employee.query("SELECT * FROM employee", (err, result) => {
+        if (err) {
             console.log(err)
-        }else{
+        } else {
             res.send(result);
         }
     });
 });
-app.put('/update',(req,res) =>{
+app.put('/update', (req, res) => {
     const id = req.body.id;
     const name = req.body.name;
     const age = req.body.age;
     const country = req.body.country;
     const position = req.body.position;
     const wage = req.body.wage;
-    employee.query("UPDATE employee SET name = ?, age = ?, country = ?, position = ?, wage = ?  WHERE id = ?",[name,age,country,position,wage,id],//fixable follow column
-    (err,result) =>{
-        if(err){
-            console.log(err);
-        }else{
-            res.send(result);
-        }
-    });
+    employee.query("UPDATE employee SET name = ?, age = ?, country = ?, position = ?, wage = ?  WHERE id = ?", [name, age, country, position, wage, id],//fixable follow column
+        (err, result) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.send(result);
+            }
+        });
 })
 
 
@@ -48,49 +46,82 @@ app.put('/update',(req,res) =>{
 //--------------------------------------------------------------------------------------------------------------------
 //create database connection employee_temp table 
 const employee_temp = mysql.createConnection({
-    user:"root",
-    host:"localhost",
-    password:"songkeit20839",
-    database:'employeesystem'
+    user: "root",
+    host: "localhost",
+    password: "songkeit20839",
+    database: 'employeesystem'
 })
 
-app.get('/employee_temp' ,(req,res) =>{
-    employee_temp.query("SELECT * FROM employee_temp",(err,result) => {
-        if(err){
+app.get('/employee_temp', (req, res) => {
+    employee_temp.query("SELECT * FROM employee_temp", (err, result) => {
+        if (err) {
             console.log(err)
-        }else{
+        } else {
             res.send(result);
         }
     });
 });
-app.put('/update_temp',(req,res) =>{
+app.put('/update_temp', (req, res) => {
     const id = req.body.id;
     const name = req.body.name;
     const age = req.body.age;
     const country = req.body.country;
     const position = req.body.position;
     const wage = req.body.wage;
-    employee_temp.query("UPDATE employee_temp SET name = ?, age = ?, country = ?, position = ?, wage = ?  WHERE id = ?",[name,age,country,position,wage,id],//fixable follow column
-    (err,result) =>{
-        if(err){
-            console.log(err);
-        }else{
-            res.send(result);
-        }
-    });
+    employee_temp.query("UPDATE employee_temp SET name = ?, age = ?, country = ?, position = ?, wage = ?  WHERE id = ?", [name, age, country, position, wage, id],//fixable follow column
+        (err, result) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.send(result);
+            }
+        });
 })
 
-app.get('/employee_temp_check_country' ,(req,res) =>{
-    employee_temp.query("SELECT * FROM employee_temp a LEFT join country on a.country = country.country WHERE country.country IS NULL",(err,result) => {
-        if(err){
+app.get('/employee_temp_check_country', (req, res) => {
+    employee_temp.query("SELECT * FROM employee_temp a LEFT join country on a.country = country.country WHERE country.country IS NULL", (err, result) => {
+        if (err) {
             console.log(err)
-        }else{
+        } else {
             res.send(result);
         }
     });
 });
 
 
+
+
+
+
+
+
+//-------------Login Excel upload-----------------------
+const login_DB = mysql.createConnection({
+    user: "root",
+    host: "localhost",
+    password: "songkeit20839",
+    database: 'employeesystem'
+})
+
+app.post('/login', (req, res) => {
+    const username = req.body.username
+    const password = req.body.password
+    login_DB.query("SELECT * FROM loginexcelupload WHERE UserName = ? AND UserPassword = ?",
+        [username, password],
+        (err, result) => {
+            if (err) {
+                res.send({err:err})
+            }
+
+            if (result.length > 0) {
+                res.send(result);
+            } else {
+                res.send({ message: "username and password don't match." });
+            }
+
+
+        });
+});
 // set port
 app.listen('3001', () => {
     console.log('server running on port 3001')
