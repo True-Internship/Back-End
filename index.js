f54daa4ceb481b2845cf659ab0aa0a589f732e6c
Songkeit <6210742083@student.tu.ac.th>
2022-07-14 22:33:37 +0700

    m

--- index.js
+++ index.js
@@ -53,8 +53,9 @@ app.put('/update', (req, res) => {
     const Nation = req.body.Nation;
     const vip = req.body.vip;
     const ConsentDM = req.body.ConsentDM;
-    employee.query("UPDATE vwemployee_cp_new SET companygroup = ?,companyname = ?,empid = ?,identification = ?,b_dd = ?,b_mm = ?,b_yyyy = ?,salutation_thai = ?,thai_firstname = ?,thai_lastname = ?,Thai_Fullname = ?,salutation_eng = ?,eng_firstname = ?,eng_lastname = ?,position = ?,email = ?,positioncode = ?,phone_No = ?,province = ?,worksite = ?,employment_Type = ?,worktype = ?,Report = ?,SalLessThan15k = ?,joindate = ?,business_SIM = ?,Nation = ?,vip = ?,ConsentDM = ? where thai_firstname = ?",
-        [companygroup,
+    employee.query("UPDATE vwemployee_cp_new SET companygroup = ?,companyname = ?,empid = ?,identification = ?,b_dd = ?,b_mm = ?,b_yyyy = ?,salutation_thai = ?,thai_firstname = ?,thai_lastname = ?,Thai_Fullname = ?,salutation_eng = ?,eng_firstname = ?,eng_lastname = ?,position = ?,email = ?,positioncode = ?,phone_No = ?,province = ?,worksite = ?,employment_Type = ?,worktype = ?,Report = ?,SalLessThan15k = ?,joindate = ?,business_SIM = ?,Nation = ?,vip = ?,ConsentDM = ? where companygroup = ? AND companyname = ?",
+        [
+            companygroup,
             companyname,
             empid,
             identification,
@@ -83,7 +84,8 @@ app.put('/update', (req, res) => {
             Nation,
             vip,
             ConsentDM,
-            thai_firstname,
+            companygroup,
+            companyname,
         ],//fixable follow column
         (err, result) => {
             if (err) {
